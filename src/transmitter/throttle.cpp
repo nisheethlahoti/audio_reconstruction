a40c029fb194c3cf7a2b1d7c55120fa107b81d75
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2018-03-18 11:40:35 +0530

    Changing throttle.cpp to make sure no two packets go out immediately

--- src/transmitter/throttle.cpp
+++ src/transmitter/throttle.cpp
@@ -7,9 +7,11 @@
 int main() {
 	set_realtime();
 	std::array<sample_t, packet_samples> buffer;
-	auto time = std::chrono::steady_clock::now();
+	std::chrono::time_point<std::chrono::steady_clock> time{};
+
 	while (std::cin.read(reinterpret_cast<char*>(&buffer), sizeof(buffer))) {
-		std::this_thread::sleep_until(time += duration);
+		time = std::max(time + duration, std::chrono::steady_clock::now());
+		std::this_thread::sleep_until(time);
 		std::cout.write(reinterpret_cast<char*>(&buffer), sizeof(buffer));
 	}
 }
