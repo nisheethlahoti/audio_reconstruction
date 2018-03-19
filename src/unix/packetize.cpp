0563cff6bb940e6e6eed696c36c343f1d81275ba
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2018-03-20 01:45:52 +0530

    Allowing 'packetize' to drop packets, and generally making its output much nicer.

--- src/unix/packetize.cpp
+++ src/unix/packetize.cpp
@@ -3,19 +3,40 @@
 #include <array>
 #include <cstring>
 #include <iostream>
+#include <random>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) try {
+	if (argc < 2)
+		throw std::runtime_error("Too few arguments");
+
 	packet_t buf{};
-	if (argc > 1)
-		buf.num = atoi(argv[1]);
+	std::default_random_engine gen(12345);
+	std::bernoulli_distribution dist(std::strtod(argv[1], &argv[1]));
 
 	set_realtime();
+	if (argc > 2)
+		buf.num = std::strtoul(argv[2], &argv[2], 0);
+
+	if (argv[1][0] || dist.p() <= 0 || dist.p() > 1 || (argc > 2 && argv[2][0]) || errno)
+		throw std::runtime_error("Invalid argument");
+
+	std::cerr << "Dropping packets with probability " << dist.p() << '\n';
+	unsigned incr = std::max(1u, unsigned(std::chrono::milliseconds(500) / duration));
+
 	while (std::cin.read(reinterpret_cast<char *>(buf.samples.data() + buf.trailing.size()),
 	                     sizeof(buf.samples))) {
-		if (buf.num++ % 1000 == 0)
-			std::cerr << "Sending next thousand packets\n";
+		if (++buf.num % incr == 0) {
+			std::cerr << "\rSending packet " << buf.num;
+			std::cerr.flush();
+		}
 
-		std::cout.write(reinterpret_cast<char *>(&buf), sizeof(buf));  // assuming little endian
+		if (!dist(gen))
+			std::cout.write(reinterpret_cast<char *>(&buf), sizeof(buf));  // assuming little endian
 		std::memcpy(buf.samples.data(), buf.trailing.data(), sizeof(buf.trailing));
 	}
+
+	std::cerr << std::endl;
+} catch (std::runtime_error &err) {
+	std::cerr << err.what() << "; Usage: " << argv[0] << " <drop_chance> [<starting_num>]\n";
+	return 1;
 }
