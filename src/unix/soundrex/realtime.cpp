1c06d224f397a49cb63cbdb307a1fe06e111d393
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2018-03-20 03:06:51 +0530

    Adding sync_with_stdio to false in realtime.cpp; in the interests of not wasting time during exec.

--- src/unix/soundrex/realtime.cpp
+++ src/unix/soundrex/realtime.cpp
@@ -7,6 +7,7 @@ void set_realtime() {
 	constexpr auto policy = SCHED_FIFO;
 	sched_param const param{sched_get_priority_min(policy) + 10};
 	pthread_setschedparam(pthread_self(), policy, &param);
+	std::ios::sync_with_stdio(false);
 	std::cin.setf(std::ios::unitbuf);
 	std::cout.setf(std::ios::unitbuf);
 }
