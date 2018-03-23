e127487f7c3759cd7966e6d1426f65dbded12d50
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2018-03-23 18:11:04 +0530

    Taking terminal input on packetize

--- src/soundrex/unix/runtime/packetize.cpp
+++ src/soundrex/unix/runtime/packetize.cpp
@@ -1,5 +1,7 @@
-#include <soundrex/constants.h>
+#include <signal.h>
+#include <soundrex/unix/lib/multiplexer.h>
 #include <soundrex/unix/runtime/lib.h>
+#include <unistd.h>
 #include <array>
 #include <cstring>
 #include <iostream>
@@ -19,17 +21,45 @@ void soundrex_main(slice_t<char *> args) {
 	std::default_random_engine gen(12345);
 	std::bernoulli_distribution dist(prob);
 
-	std::clog << "Dropping packets with probability " << dist.p() << std::endl;
+	std::clog << "Will drop with probability " << dist.p() << " when required" << std::endl;
 	unsigned incr = std::max(1u, unsigned(std::chrono::milliseconds(500) / duration));
+	bool extra_drops = false;
 
-	while (buf_read_blocking(buf.samples.data() + buf.trailing.size(), sizeof(buf.samples))) {
-		if (++buf.num % incr == 0)
-			std::clog << "\rSending packet " << buf.num << std::flush;
+	multiplexer_t multiplexer;
+	multiplexer.add_fd(0);  // For incoming sample stream
+	multiplexer.add_fd(2);  // For terminal user input
 
-		if (!dist(gen))
-			buf_write(&buf, sizeof(buf));  // assuming little endian
-		std::memcpy(buf.samples.data(), buf.trailing.data(), sizeof(buf.trailing));
+	uint8_t *const bstart = reinterpret_cast<uint8_t *>(buf.samples.data() + buf.trailing.size());
+	uint8_t *const bend = reinterpret_cast<uint8_t *>(buf.trailing.end());
+	uint8_t *pos = bstart;
+
+	while (multiplexer.next(), true) {
+		if (multiplexer.is_ready(0)) {
+			if (size_t done = buf_read_available(pos, bend - pos); !done) {
+				std::clog << "End of input stream" << std::endl;
+				break;
+			} else if ((pos += done) == bend) {
+				if (++buf.num % incr == 0)
+					std::clog << buf.num << " sent\r" << std::flush;
+
+				if (!extra_drops || !dist(gen))
+					buf_write(&buf, sizeof(buf));  // assuming little endian
+
+				std::memcpy(buf.samples.data(), buf.trailing.data(), sizeof(buf.trailing));
+				pos = bstart;
+			}
+		} else {
+			char c;
+			wrap_error(read(2, &c, 1), "reading tty character");
+			if (c == 't') {
+				extra_drops = !extra_drops;
+				std::clog << "Extra drops " << (extra_drops ? "on" : "off") << std::endl;
+			} else if (c == '\r' || c == '\n')
+				kill(0, SIGURG);  // TODO: Find less horrible way that this.
+			else if (c == 'q')
+				break;
+			else
+				std::clog << "Unrecognized input " << int(c) << std::endl;
+		}
 	}
-
-	std::clog << std::endl;
 }
