86ccd4b56899dd808084a3fdd33da9ba527acac5
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2018-03-27 12:28:58 +0530

    Adding .bin files and compile_commands.json in gitignore + deleting ycm_conf

--- src/.ycm_extra_conf.py
+++
@@ -1,7 +0,0 @@
-import os 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-def FlagsForFile( filename, **kwargs ):
-	return {
-		'flags': [ '-x', 'c++', '-std=gnu++1z', '-Wall',  '-Werror', '-I', dir_path],
-	}
