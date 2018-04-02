442a3b71a4648c29adcb2513f8071d0ff9e6855d
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2018-04-03 00:50:16 +0530

    fixing bug in buf_read

--- src/soundrex/unix/lib.cpp
+++ src/soundrex/unix/lib.cpp
@@ -10,10 +10,13 @@ size_t buf_read_available(void *buf, size_t count) {
 	return static_cast<size_t>(wrap_error(read(0, buf, count), "reading input"));
 }
 
-bool buf_read_blocking(void *buf, size_t count) {
-	while (size_t done = buf_read_available(buf, count))
+bool buf_read_blocking(void *const buf, size_t count) {
+	char *buffer = static_cast<char *>(buf);
+	while (size_t done = buf_read_available(buffer, count)) {
+		buffer += done;
 		if ((count -= done) == 0)
 			return true;
+	}
 	return false;
 }
 
